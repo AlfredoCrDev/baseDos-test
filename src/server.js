deafbd41b9e786adafba76310d6c76799951cb9d
Alfredo Carrasquel <alfreocrdev@gmail.com>
2024-05-13 18:17:07 -0400

    Instalacion y configuracion de swagger y se crea archivo yaml de productos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
-const express  = require('express');
-const app      = express();
-const PORT     = process.env.PORT || 3001;
+const express      = require('express');
+const app          = express();
+const PORT         = process.env.PORT || 3001;
+const swaggerUi    = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc')
+
 require('dotenv').config();
 
 
@@ -10,10 +13,24 @@ const productRoutes =  require("./routes/product.routes.js");
 
 connectDB();
 
+const swaggerOptions = {
+    definition:{
+        openapi: '3.0.1',
+        info:{
+            title:'Documentacion API',
+            description: "Api clase swagger"
+        }
+    },
+    apis: ['./src/docs/**/*.yaml']
+}
+
+const specs = swaggerJSDoc(swaggerOptions)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/products", productRoutes)
+app.use("/apidocs", swaggerUi.serve, swaggerUi.setup(specs))
 
 const server = app.listen(PORT, () => {
     console.log(`Servidor Corriendo en http://localhost:${PORT}`)
